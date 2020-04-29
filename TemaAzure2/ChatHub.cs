06e586a112421c2e63ff9077f9d302cf96079544
MihaelaPotlog <mihaela.potlog@info.uaic.ro>
2020-04-29 22:30:11 +0300

    authentication

---
+++ TemaAzure2/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TemaAzure2
+{
+    public class ChatHub:Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
---
+++ TemaAzure2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemaAzure2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
